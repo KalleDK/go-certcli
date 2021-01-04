d07b8397b7371138e7b260e50084114336e8fcea
Kalle Møller Aagaard (KM) <km@conscia.com>
2021-01-04 13:08:23 +0100

    Update entrypoint.sh

--- .github/actions/alpine/entrypoint.sh
+++ .github/actions/alpine/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-addgroup -g $(stat -c "%g" .) docker
-adduser -D -G docker -g 'Alpine Package Builder' -u $(stat -c "%u" .) -s /bin/ash runner 
-adduser runner abuild
+#addgroup -g $(stat -c "%g" .) docker
+#adduser -D -G docker -g 'Alpine Package Builder' -u $(stat -c "%u" .) -s /bin/ash runner 
+#adduser runner abuild
 export REPODEST="${GITHUB_WORKSPACE}/packages"
 export SRCDEST="${GITHUB_WORKSPACE}/cache/distfiles"
-su runner -c 'abuild-keygen -n -a'
-su runner -c 'abuild checksum'
-su runner -c 'abuild -r'
+export PACKAGER_PRIVKEY="/root/key.rsa"
+printf "${INPUT_ABUILD_KEY}" > "${PACKAGER_PRIVKEY}"
+#su runner -c 'abuild-keygen -n -a'
+#su runner -c 'abuild checksum'
+#su runner -c 'abuild -r'
+abuild -F checksum
+abuild -F -r
