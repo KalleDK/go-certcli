26648557511a491c88cae17893de9e962e76d906
Kalle Møller Aagaard (KM) <km@conscia.com>
2021-01-04 13:42:02 +0100

    more sign

--- .github/actions/alpine/entrypoint.sh
+++ .github/actions/alpine/entrypoint.sh
@@ -5,10 +5,14 @@
 #adduser runner abuild
 export REPODEST="${GITHUB_WORKSPACE}/packages"
 export SRCDEST="${GITHUB_WORKSPACE}/cache/distfiles"
-export PACKAGER_PRIVKEY="/root/key.rsa"
+export PACKAGER_PRIVKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa"
+export PACKAGER_PUBKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
 printf "${INPUT_ABUILD_KEY}" > "${PACKAGER_PRIVKEY}"
+printf "${INPUT_ABUILD_KEY_PUB}" > "${PACKAGER_PUBKEY}"
+cp "${PACKAGER_PUBKEY}" /etc/apk/keys/
 #su runner -c 'abuild-keygen -n -a'
 #su runner -c 'abuild checksum'
 #su runner -c 'abuild -r'
 abuild -F checksum
 abuild -F -r
+apk verify $REPODEST/x86_64/certcli-*
